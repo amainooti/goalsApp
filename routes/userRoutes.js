5bb199a37e4faf27bce34fe7fab3bad2d6b144d3
amainooti <amainooti@gmail.com>
2022-07-10 16:40:55 +0100

    created a secured and private route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const {registerUser, loginUser, getUser} = require("../controller/userController")
+const {protect} = require("../middleware/authMiddleware")
 
 router.post("/", registerUser)
 router.post("/login", loginUser)
-router.get("/user", getUser)
+router.get("/user",protect, getUser)
 
 module.exports = router
